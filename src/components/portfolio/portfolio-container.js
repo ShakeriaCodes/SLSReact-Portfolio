b00555e1f064f7fff7c6364f2f79cae5a29a60a4
ShakeriaCodes <yogi_keri@icloud.com>
2019-05-22 13:49:59 -0700

    added multiple new files

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,30 +1,62 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import PortfolioItem from "./portfolio-item";
-
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
-
-        console.log("Portfolio containr has rendered");
+  constructor() {
+    super();
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://shakeriasmith.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
-        const data =["Quip","Eventbrite", "Ministry Safes", "SwingAway"]
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("About")}>
+          About
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Contact")}>
+          Contact
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Blog")}>
+          Blog
+        </button>
 
-        return data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"}/>;
-        });  
-    }
-
-    render() {
-        return (
-
-            <div>
-                <h2>Portfolio items go here...</h2>
-
-                {this.portfolioItems()}
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
---
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const PortfolioSidebarList = props => {
+  const portfolioList = props.data.map(portfolioItem => {
+    return (
+        <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}
+          >
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;;
+};
+
+export default PortfolioSidebarList;
